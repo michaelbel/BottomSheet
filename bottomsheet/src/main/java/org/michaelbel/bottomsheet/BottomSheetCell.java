c8d34a473006c4d8b4c8d6aa55197e7dff6593fc
Michael Bel <michaelbel@protonmail.com>
2016-10-30 21:18:34 +0300

    Add license

--- bottomsheet/src/main/java/org/michaelbel/bottomsheet/BottomSheetCell.java
+++ bottomsheet/src/main/java/org/michaelbel/bottomsheet/BottomSheetCell.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Michael Bel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.michaelbel.bottomsheet;
 
 import android.content.Context;
@@ -15,6 +31,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+@SuppressWarnings("all")
 public class BottomSheetCell extends FrameLayout {
 
     private TextView textView;
@@ -96,6 +113,16 @@ public class BottomSheetCell extends FrameLayout {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(Utils.dp(getContext(), 48), MeasureSpec.EXACTLY));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = MeasureSpec.makeMeasureSpec(widthMeasureSpec, MeasureSpec.EXACTLY);
+        int height = Utils.dp(getContext(), 48);
+
+        setMeasuredDimension(width, height);
     }
+
+    /*@Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(Utils.dp(getContext(), 48), MeasureSpec.EXACTLY));
+    }*/
 }
