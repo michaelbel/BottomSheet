0087e0129cb33d9b7e4c8681f26e78e1bc160340
Michael Bel <michael-bel@outlook.com>
2018-03-03 14:02:04 +0300

    Add BottomSheet item class

---
+++ bottomsheet/src/main/java/org/michaelbel/bottomsheet/BottomSheetItem.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016-2018 Michael Bel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.michaelbel.bottomsheet;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Date: Sut, Mar 3 2018
+ * Time: 13:58 MSK
+ *
+ * @author Michael Bel
+ */
+
+@SuppressWarnings("all")
+public class BottomSheetItem {
+
+    public Drawable icon;
+    public CharSequence text;
+
+    public BottomSheetItem() {}
+
+    public BottomSheetItem(Drawable icon, CharSequence text) {
+        this.icon = icon;
+        this.text = text;
+    }
+}
